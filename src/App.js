46e4925436abbdd9d935a7c9134e3a393dc9d8c2
manan999 <aggarwalmanan4@gmial.com>
2020-08-09 13:26:34 +0530

    commit 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component
+{ render()
+  {
+    return (
+      <div className="App">
+        This is an App
+      </div>
+    ); 
+  }
+}
+
+export default App;
